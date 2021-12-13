4cccbe7a711ea44f9747aad023a43f6d3ae7b962
sudha <sudha@DESKTOP-J4BO6EL>
2021-12-13 13:16:33 +0530

    New class added

---
+++ src/main/java/com/Vtiger/POMclasses/HomePage.java
@@ -0,0 +1,63 @@
+package com.Vtiger.POMclasses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.Vtiger.genericUtil.WebdriverUtil;
+
+public class HomePage  //Rule 1
+{
+	
+	WebDriver driver;
+	
+	@FindBy(xpath="//a[text()='Sign Out']")
+	private WebElement signoutlink;
+	
+	
+	 @FindBy(xpath="//a[text()='Organizations']")//Rule 2
+	 private WebElement orglink;
+	 
+	 @FindBy(xpath="//a[text()='Contacts']")
+	 private WebElement contactlink;
+	 
+	 @FindBy(xpath="//img[@src='themes/softed/images/user.PNG']")
+	 private WebElement logoutimg;
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	public WebElement getSignoutlink() {
+		return signoutlink;
+	}
+
+	public WebElement getOrglink() {
+		return orglink;
+	}
+
+	public WebElement getContactlink() {
+		return contactlink;
+	}
+
+	public WebElement getLogoutimg() {
+		return logoutimg;
+	}
+
+	public HomePage(WebDriver driver) {
+     this.driver=driver;
+     PageFactory.initElements(driver, this);
+
+	}
+	
+	public void logOutfromApp() {
+		WebdriverUtil webutil=new WebdriverUtil(driver);
+		webutil.movetoElemnt(logoutimg);
+		signoutlink.click();
+}
+	 
+	 
+	 
+
+}
